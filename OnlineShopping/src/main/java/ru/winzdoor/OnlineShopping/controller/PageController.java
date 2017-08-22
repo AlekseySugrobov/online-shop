ad763c54762d9b1f9fd93ed364d08a183e739d50
AlekseySugrobov <Aleksey.Sugrobov@gmail.com>
2017-08-22 22:02:41 +0500

    add bootstrap and configure static resources

--- OnlineShopping/src/main/java/ru/winzdoor/OnlineShopping/controller/PageController.java
+++ OnlineShopping/src/main/java/ru/winzdoor/OnlineShopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package ru.winzdoor.OnlineShopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,6 +12,25 @@ public class PageController {
     public ModelAndView index(){
         ModelAndView mv = new ModelAndView("page");
         mv.addObject("greeting", "welcome to spring web mvc");
+        return mv;
+    }
+
+    @RequestMapping(value = "/test_request")
+    public ModelAndView test_request(@RequestParam(value = "greeting", required = false) String greeting){
+        return getModelAndView(greeting);
+    }
+
+    @RequestMapping(value = "/test_path/{greeting}")
+    public ModelAndView test_path(@PathVariable(value = "greeting")String greeting){
+        return getModelAndView(greeting);
+    }
+
+    private ModelAndView getModelAndView(String greeting) {
+        if(greeting == null){
+            greeting = "'greeting' is null";
+        }
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("greeting", greeting);
         return mv;
     }
 }
